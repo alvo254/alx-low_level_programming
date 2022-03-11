53ee7447e27524a72a82abf2daadcbe4ff978161
alvo254 <alvinkhamron@gmail.com>
2022-03-11 19:37:15 +0300

    insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,11 +12,6 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
     char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
