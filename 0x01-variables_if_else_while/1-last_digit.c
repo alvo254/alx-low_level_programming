a6203aa66706bc92f44bdcfd39647eb6df63631f
alvo254 <alvinkhamron@gmail.com>
2022-03-11 19:09:48 +0300

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,13 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+
 /* betty style doc for function main goes there */
 int main(void)
 {
