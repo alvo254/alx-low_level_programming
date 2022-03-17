816107b047b7eec3260e208944e68f403c04b20f
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-03-17 11:33:49 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,16 @@
 #include "main.h"
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-#include <stdio.h>
 
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
- * Return: Always 0 (Success)
- */
+  * main - Test function for positive or negative
+  * Return: 0
+  */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	return (0);
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
 }
